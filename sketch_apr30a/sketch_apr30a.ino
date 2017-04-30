9ae12b53705b48d696e2da0c503de3f40948072f
ssuhrid <suhrid.srivastava@gmail.com>
2017-04-30 13:40:47 +0530

    MSB will print if num exceeds digit

--- sketch_apr30a/sketch_apr30a.ino
+++ sketch_apr30a/sketch_apr30a.ino
@@ -9,10 +9,10 @@ int data=2;
 int clk=3;
 int strobe=4;
 
-int digits=3;
+int digits=4;
+int nums=4; //total number to be printed
 
-void setup()
-{
+void setup(){
   Serial.begin(9600);
   Serial.print("Start");
   pinMode(strobe,OUTPUT);
@@ -20,6 +20,34 @@ void setup()
   pinMode(clk,OUTPUT);
 }
 
+void writeNum(unsigned long num, uint8_t c){
+  int arr[digits];
+  int len=0,x=num;
+  while (x!=0){
+    len++;
+    x/=10;
+  }
+  Serial.println(len);
+  if (len <= digits){
+    x=num;
+    for (int i=0;i<digits;i++){
+      if (i>=len){
+        arr[i]=10;
+      }
+      else{
+        arr[i]=x%10;
+        x/=10;
+      }
+    }
+    arr[c]=10;
+    writeArray(arr);
+  }
+  else{
+    Serial.print(num);
+    Serial.println("num larger than digits");
+  }
+
+}
 void writeNum(unsigned long num){
   int arr[digits];
   int len=0,x=num;
@@ -44,6 +72,19 @@ void writeNum(unsigned long num){
   else{
     Serial.print(num);
     Serial.println("num larger than digits");
+    x=num;
+    unsigned long y=0;
+    while(x!=0){
+      y=(y*10)+x%10;
+      Serial.println(y);
+      x/=10;
+    }
+    Serial.println(y);
+    for(int i=0;i<digits;i++){
+      arr[digits-1-i]=y%10;
+      y/=10;
+    }
+    writeArray(arr);
   }
 }
 void writeArray(int arr[]){
@@ -66,9 +107,14 @@ void test(){
   }
 }
 
-void loop(){
-  //test();
-  writeNum(124);
+void writeNumbers(int num[]){
+  int arr[digits];
+  for(int i=0;i<nums;i++)
+    Serial.println(num[i]);
+}
+
+void checkUP(){
+  writeNum(12345);
   delay(1000);
   writeNum(645);
   delay(1000);
@@ -76,4 +122,10 @@ void loop(){
   delay(1000);
   writeNum(90);
   delay(1000);
+}
+void loop(){
+  checkUP();
+  //test();
+  //int arr[]={2,3,4,5};
+  //writeNumbers(arr);
 }
